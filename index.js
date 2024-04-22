d93adb8ba66e6fdd06d42b4fcb89861ca35fd3e2
meheraz1100 <meheraz1100@gmail.com>
2024-04-22 10:37:38 +0600

    more and more practice required

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const users = [
+    {id: 1, name: "Meheraz", email: "meheraz@gmail.com"},
+    {id: 2, name: "Mosaiyeb", email: "mosaiyeb@gmail.com"},
+    {id: 3, name: "islam", email: "islam@gmail.com"}
+]
+
+app.get('/', (req, res) => {
+    res.send('Users management server is running')
+})
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.post('/users', (req, res) => {
+    console.log('post api hitting')
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id = users.length + 1
+    users.push(newUser)
+    res.send(newUser)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
---
+++ steps.js
@@ -0,0 +1,9 @@
+/** 
+ * create a post api on server side
+ * send data from client side via post
+ * set fetch method inside the fetch options (second parameter)
+ * options will have three things : method 
+ * options will have headers: "content-type": "application/json"
+ * don't forget to send data by JSON.stringify in the body
+ * On the server side : don't forget to use expree.json()
+*/
